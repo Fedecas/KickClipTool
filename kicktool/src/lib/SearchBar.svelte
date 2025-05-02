488d082a0933a47ea80ed5bc3f5c01d17f728a92
Fedecas <elfefecabjsape@gmail.com>
2025-05-02 19:18:55 -0300

    Refactor SearchBar component to use Spinner and Search components

--- kicktool/src/lib/SearchBar.svelte
+++ kicktool/src/lib/SearchBar.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import Spinner from './Spinner.svelte';
+  import { Search } from 'lucide-svelte';
+
   let { value = $bindable(), searching, handle_search, hasResults } = $props()
 </script>
 
@@ -12,11 +15,9 @@
   />
   <span class="absolute inset-y-0 left-4 flex items-center">
     {#if searching}
-    <div class="animate-spin rounded-full h-6 w-6 border-t-2 border-gray-500"></div>
+    <Spinner />
     {:else}
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-    </svg>
+    <Search />
     {/if}
   </span>
 </div>
