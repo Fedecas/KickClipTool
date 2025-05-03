b7b6062a5451d3b19e64ffe58270589ed369e746
Fedecas <elfefecabjsape@gmail.com>
2025-05-03 15:45:15 -0300

    Refactor searchChannel function for clarity and consistency

--- svelte-port/src/lib/api.ts
+++ svelte-port/src/lib/api.ts
@@ -1,15 +1,15 @@
 export async function searchChannel(query: string): Promise<any[]> {
-  if (!query) return []
+  let result: any[] = [];
+  if (query) {
+    const params = new URLSearchParams({'searched_word': query});
 
-  let result: any[] = []
-  const params = new URLSearchParams({'searched_word': query})
-
-  try {
-    const response = await fetch(`/api/channels?${params}`)
-    result = await response.json()
-  } catch (error) {
-    console.error('Error fetching data:', error)
+    try {
+      const response = await fetch(`/api/channels?${params}`);
+      result = await response.json();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 
-  return result
+  return result;
 }
--- svelte-port/src/routes/api/channels/+server.ts
+++ svelte-port/src/routes/api/channels/+server.ts
@@ -3,7 +3,7 @@ import { json } from '@sveltejs/kit'
 const CHANNELS_ENDPOINT = 'https://kick.com/api/search'
 
 export async function GET({ url }): Promise<Response> {
-  const requestUrl: string = CHANNELS_ENDPOINT + '?' + url.searchParams
+  const requestUrl: string = CHANNELS_ENDPOINT + '?' + url.searchParams;
   const response: Response = await fetch(requestUrl, {
     headers: {
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome",
@@ -15,12 +15,12 @@ export async function GET({ url }): Promise<Response> {
     method: 'GET',
     mode: 'cors',
     credentials: 'omit'
-  })
+  });
 
   if (response.ok) {
-    const content = await response.json()
-    return json(content.channels)
+    const content = await response.json();
+    return json(content.channels);
   } else {
-    throw new Error(`Network response was not ok (${response.status})`)
+    throw new Error(`Network response was not ok (${response.status})`);
   }
 }
