8ac51df9a1f5ca885599455f8f82712a73cb6fc6
Fedecas <elfefecabjsape@gmail.com>
2025-05-03 18:56:18 -0300

    Refactor Channel component to display user avatar and follower count

--- svelte-port/src/lib/Channel.svelte
+++ svelte-port/src/lib/Channel.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
-  let { product } = $props();
+  import { BadgeCheck } from 'lucide-svelte';
+
+  let { channel } = $props();
+
+  const user: any = channel?.user || {};
+
+  const avatar: string = user.profilePic || randomAvatar();
+  const followers: number = channel?.followersCount || 0;
+  const name: string = user.username || '';
+  const verified: boolean = !!channel?.verified;
+  //const slug: string = channel?.slug || '';
+
+  function randomAvatar(): string {
+    const AVATARS: number = 6;
+    const n: number = Math.floor(Math.random() * AVATARS) + 1;
+    return `/default_avatars/${n}.jpeg`;
+  }
 </script>
 
-<div class="outline h-64">
-  <h1 class="text-3xl font-medium">{product.slug}</h1>
-  <h2 class="text-xl font-bold"> ({product.slug}€)</h2>
+<div class="outline flex flex-col items-center rounded-md gap-1">
+  <img class="w-96 h-64 object-cover" src={avatar} alt="Channel avatar" />
+  <span class="flex flex-row">
+    <h1 class="text-2xl mt-1">{name}</h1>
+    {#if verified}
+    <BadgeCheck class="text-(--primary) ml-2 mt-2"/>
+    {/if}
+  </span>
+  <h2 class="font-medium text-(--primary) mb-1">{followers} followers</h2>
+  <!--<h1 class="text-md font-medium">{verified}</h1>-->
 </div>
