bb23779773366ff1fcd57e959d234c6880eed2f7
Fedecas <elfefecabjsape@gmail.com>
2025-05-05 19:57:02 -0300

    Add date-fns for date formatting and enhance Clip component layout

--- svelte-port/src/lib/Clip.svelte
+++ svelte-port/src/lib/Clip.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { Eye } from 'lucide-svelte';
+  import { Eye, Calendar } from 'lucide-svelte';
+  import { formatDistance } from 'date-fns';
 
   let { clip } = $props();
 
@@ -16,6 +17,12 @@
     const seconds: string = `${duration % 60}`;
     return `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}`;
   };
+
+  function formatDate(date: string): string {
+    const clipDate: Date = new Date(date);
+    const today: Date = new Date();
+    return formatDistance(today, clipDate);
+  }
 </script>
 
 <button
@@ -26,13 +33,16 @@
     src={thumbnail}
     alt="Clip thumbnail"
     class="w-full rounded-sm object-cover"/>
-    <h1 class="absolute bg-black/70 rounded-sm text-sm left-1 top-1 p-1.5">{formatDuration(duration)}</h1>
+    <h3 class="absolute bg-black/70 rounded-sm text-sm left-1 top-1 p-1.5">{formatDuration(duration)}</h3>
     <span class="absolute bg-black/60 rounded-sm right-1 bottom-1 p-1.5 flex flex-row">
-      <h1 class="text-sm font-bold">{views}</h1>
+      <h3 class="text-sm font-bold">{views}</h3>
       <Eye class="ml-1 h-5.5" />
     </span>
   </span>
   <h1 class="content-center text-xl/4 font-medium leading-none line-clamp-2 h-12">{title}</h1>
-  <h1 class="text-(--primary) font-medium">{creator}</h1>
-  <h1 class="">{date}</h1>
+  <h2 class="text-(--primary) font-medium">{creator}</h2>
+  <span class="items-center justify-center flex flex-row">
+    <Calendar class="text-(--secondary) mr-1 h-4.5" />
+    <h3 class="text-(--secondary)">{formatDate(date)}</h3>
+  </span>
 </button>
