c16853d19cd22744566bf2744ca48c1a942b8684
Fedecas <elfefecabjsape@gmail.com>
2025-05-10 02:34:58 -0300

    Refactor Channel component markup for improved readability

--- svelte-port/src/lib/Channel.svelte
+++ svelte-port/src/lib/Channel.svelte
@@ -22,23 +22,21 @@
   }
 </script>
 
-<div class="group">
-  <button
-    type="button"
-    onclick={() => handleClick({name, slug})}
-    class="outline outline-(color:--secondary) p-1 flex flex-col items-center rounded-md gap-1
-           transition duration-150 ease-in-out group-hover:scale-101 group-hover:outline-2
-           group-hover:outline-(--primary) group-hover:shadow-lg shadow-(color:--primary)">
-    <img
-      src={validAvatar}
-      alt="{name} avatar"
-      class="w-96 h-64 object-cover rounded-md"/>
-    <span class="flex flex-row">
-      <h1 class="text-2xl mt-1">{name}</h1>
-      {#if verified}
-      <BadgeCheck class="text-(--primary) ml-2 mt-2"/>
-      {/if}
-    </span>
-    <h2 class="font-medium text-(--primary) mb-1">{followers} followers</h2>
-  </button>
-</div>
+<button
+  type="button"
+  onclick={() => handleClick({name, slug})}
+  class="outline outline-(color:--secondary) p-1 flex flex-col items-center rounded-md gap-1
+          transition duration-150 ease-in-out hover:scale-101
+          hover:outline-2 hover:outline-(--primary) hover:shadow-lg shadow-(color:--primary)">
+  <img
+    src={validAvatar}
+    alt="{name} avatar"
+    class="w-96 h-64 object-cover rounded-md"/>
+  <span class="flex flex-row">
+    <h1 class="text-2xl mt-1">{name}</h1>
+    {#if verified}
+    <BadgeCheck class="text-(--primary) ml-2 mt-2"/>
+    {/if}
+  </span>
+  <h2 class="font-medium text-(--primary) mb-1">{followers} followers</h2>
+</button>
