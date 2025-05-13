5e25fde73f8139fc5430135e1225f3adffed8869
Fedecas <elfefecabjsape@gmail.com>
2025-05-12 21:12:23 -0300

    Enhance Logo style

--- svelte-port/src/lib/Logo.svelte
+++ svelte-port/src/lib/Logo.svelte
@@ -13,11 +13,11 @@
   in:blur={{ duration: 1000 }}
   class="flex flex-col items-center">
   <img
-    class="w-64 transition-all {hasResults ? 'mt-12' : 'mt-32'}"
+    class="w-64 drop-shadow-lg/100 transition-margin duration-500 {hasResults ? 'mt-12' : 'mt-32'}"
     src="/kick-logo.svg"
     alt="Kick logo"
   />
-  <h1 class="text-3xl font-medium transition-all {hasResults ? 'invisible' : 'mb-2'}">
+  <h1 class="text-3xl font-medium {hasResults ? 'invisible' : 'mb-2'}">
     Clip Tool
   </h1>
 </div>
