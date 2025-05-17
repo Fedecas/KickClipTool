8e1d03c1bced78f1c0297f16b5dab9a410003a62
Fedecas <elfefecabjsape@gmail.com>
2025-05-16 22:45:26 -0300

    Initialize Tauri application with Rust backend and Svelte frontend. Adapt Vite and Node configuration

--- svelte-port/vite.config.ts
+++ svelte-port/vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		tailwindcss()
-	]
+	],
+  server: {
+    watch: {
+      ignored: ['**/tmp/**'],
+    },
+  },
 });
