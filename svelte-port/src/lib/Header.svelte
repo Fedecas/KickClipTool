9a83158584ce1537512ace7a9cde2b06f4db56be
Fedecas <elfefecabjsape@gmail.com>
2025-05-19 18:48:02 -0300

    Refactor Header component layout and styling for improved responsiveness and loading state

--- svelte-port/src/lib/Header.svelte
+++ svelte-port/src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { blur } from "svelte/transition";
+    import Spinner from "./Spinner.svelte";
 
   interface Props {
     hasResults: boolean;
@@ -7,16 +8,27 @@
 
   // Runes
   let { hasResults }: Props = $props();
+  let loadedImg = $state(false);
 </script>
 
 <div
   in:blur={{ duration: 1000 }}
-  class="flex flex-col items-center">
-  <img
-    class="w-64 drop-shadow-lg/100 transition-margin duration-500 {hasResults ? 'mt-12' : 'mt-32'}"
-    src="/kick-logo.svg"
-    alt="Kick logo"
-  />
+  class="flex flex-col items-center justify-end
+          {hasResults ? 'mt-[8dvh]' : 'mt-[16dvh]'}
+          transition-margin duration-500 ease-in-out">
+  <div class="relative w-2xs flex justify-center items-center">
+    <img
+      src="/kick-logo.svg"
+      alt="Kick logo"
+      onload={() => { loadedImg = true }}
+      class="size-full drop-shadow-lg/100 
+              {loadedImg ? 'opacity-100' : 'opacity-0'} transition-opacity duration-1000 ease-in"/>
+    {#if !loadedImg}
+    <div class="absolute w-[20%] flex items-center">
+      <Spinner />
+    </div>
+    {/if}
+  </div>
   <h1 class="text-3xl font-medium {hasResults ? 'invisible' : 'my-2'}">
     Clip Tool
   </h1>
