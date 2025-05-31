55d6ff7870e7a0efde4ec54d5b52735df67910bb
Fedecas <elfefecabjsape@gmail.com>
2025-05-31 09:10:05 -0300

    chore: update version to 2.1.0 and add build targets for node and tauri

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,29 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterNode from '@sveltejs/adapter-node';
+
+const target = process.env.BUILD_TARGET || 'node';
+const adapter =
+  target === 'tauri'
+    ? adapterStatic({
+        pages: 'build',
+        assets: 'build',
+        fallback: '200.html',
+        precompress: false,
+        strict: true
+      })
+    : adapterNode({  // Node by default
+        out: 'build',
+        precompress: true,
+        envPrefix: ''
+      });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      fallback: '200.html',
-    })
-  }
+  kit: { adapter }
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		tailwindcss()
-	]
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    sveltekit(),
+    tailwindcss()
+  ],
+  build: {
+    rollupOptions: {
+      external: mode === 'tauri' ? ['ffmpeg-static'] : ['@tauri-apps/plugin-fs']
+    }
+  }
+}));
