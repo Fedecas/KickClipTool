2d5770b552c86d8952fcfd392fb969be1d8bcc9f
Fedecas <elfefecabjsape@gmail.com>
2025-06-19 15:40:42 -0300

    refactor: improve formatting and readability in Channel component

--- src/lib/content/Channel.svelte
+++ src/lib/content/Channel.svelte
@@ -38,8 +38,9 @@
   type="button"
   onclick={onClick}
   class="flex flex-col group items-center rounded-sm gap-1 p-1
-         bg-[#242428] inset-shadow-sm/100 shadow-sm/100
-         hover:bg-gray-800 hover:inset-shadow-none hover:shadow-none">
+    bg-[#242428] inset-shadow-sm/100 shadow-sm/100
+    hover:bg-gray-800 hover:inset-shadow-none hover:shadow-none"
+>
   <div class="relative w-full h-64 bg-black/40">
     <img
       src={validAvatar}
@@ -47,9 +48,10 @@
       loading="lazy"
       onload={() => { loadedImg = true }}
       class="size-full object-cover rounded-sm brightness-70
-              {loadedImg ? 'opacity-100' : 'opacity-0 drop-shadow-md/100'}
-              transition-opacity duration-1000 ease-in
-              group-hover:brightness-100 group-hover:drop-shadow-md/100"/>
+        transition-opacity duration-1000 ease-in
+        group-hover:brightness-100 group-hover:drop-shadow-md/100
+        {loadedImg ? 'opacity-100' : 'opacity-0 drop-shadow-md/100'}"
+    />
     {#if !loadedImg}
     <div class="absolute w-[20%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Spinner />
@@ -62,5 +64,7 @@
     <BadgeCheck class="size-6 text-(--primary) mt-1 flex-shrink-0"/>
     {/if}
   </div>
-  <h2 class="font-medium text-(--primary) mb-1">{followers} followers</h2>
+  <h2 class="font-medium text-(--primary) mb-1">
+    {followers} followers
+  </h2>
 </button>
