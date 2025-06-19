1b85f00468345d87503585531d32bd1579892b14
Fedecas <elfefecabjsape@gmail.com>
2025-06-18 21:26:51 -0300

    refactor: enhance ContentState class and update Content component for improved state management

--- src/lib/Content-state.svelte.ts
+++ src/lib/Content-state.svelte.ts
@@ -1,28 +1,64 @@
-import { searchChannels, searchClips } from '$lib/api';
+import type { ChannelObject, ChannelRef, ClipObject, SortType } from './types';
+import { searchChannels } from '$lib/api';
 import { getContext, setContext } from 'svelte';
-import type { ChannelObject, ClipObject, SortType } from './types';
 
 const CONTENT_KEY = Symbol('content');
 
-export class Content {
+export class ContentState {
+  downloads = $state([]);
   channels: ChannelObject[] = $state([]);
   clips: ClipObject[] = $state([]);
   sort: SortType = $state('date');
+  channelSelected = $state(false);
+  clipSelected = $state(false);
+
+  //channel: ChannelRef | null = null;
   lastSort: SortType = 'date';
 
-  async searchChannels(value: string) {
+  hasResults = $derived(this.channels.length > 0 || this.clips.length > 0);
+  endReached = $derived(this.channelSelected &&/*channelRef.nextCursor === ''*/ this.sort === this.lastSort);
+
+  constructor() {}
+
+  searchChannels = async (value: string) => {
     this.channels = [];
     this.lastSort = this.sort;
     this.sort = 'date';
-
     this.channels = await searchChannels(value);
+  }
+
+  searchClips = async (ref: ChannelRef) => {
+    /*
+    if (this.endReached) {
+      notif.success('No more clips');
+      return;
+    }
+
+    if (channelRef !== channel) {
+      channelRef = channel;
+      results = [];
+    } else if (sort != lastSort) {
+      channelRef.nextCursor = '';
+      results = [];
+    }
+
+    const slug = channelRef.slug;
+    const cursor = channelRef.nextCursor || '';
+    const res = await searchClips(slug, cursor, sort);
+    const clips = results as ClipObject[];
+    results = [
+      ...clips,
+      ...res.clips.filter(c1 => !clips.some(c2 => c1.id === c2.id))
+    ];
+    channelRef.nextCursor = res.nextCursor;
+    */
   }
 }
 
-export function getContentContext(): Content {
-  return getContext(CONTENT_KEY) as Content;
+export function getContentState(): ContentState {
+  return getContext(CONTENT_KEY) as ContentState;
 }
 
-export function setContentContext(c: Content): Content {
-  return setContext(CONTENT_KEY, c);
+export function setContentState(cs: ContentState): ContentState {
+  return setContext(CONTENT_KEY, cs);
 }
--- src/lib/content/Content.svelte
+++ src/lib/content/Content.svelte
@@ -1,59 +1,52 @@
 <script lang="ts">
   import { onDestroy } from 'svelte';
 
-  import type { ChannelObject, ChannelRef, ClipObject, ClipRef, SortType } from '$lib/types';
+  import type { ChannelObject, ChannelRef, ClipObject, ClipRef } from '$lib/types';
   import Channel from './Channel.svelte';
   import Sort from './Sort.svelte';
   import Clip from './Clip.svelte';
 
-  interface Props {
-    results: ClipObject[] | ChannelObject[],
-    hasResults: boolean,
-    channelRef: ChannelRef | null,
-    getClips: (channel: ChannelRef, sort: SortType) => Promise<void>,
-    clipRef: ClipRef | null,
-    sort: SortType
-  }
+  import { getContentState } from '$lib/Content-state.svelte';
+  import { getSearchBarState } from '$lib/SearchBar-state.svelte';
 
-  // Constants
   const COOLDOWN_MS = 1000;
   const THRESHOLD_PX = 250;
 
-  // Runes
-  let {
-    results, hasResults, channelRef, getClips, clipRef = $bindable(), sort = $bindable()
-  }: Props = $props();
+  const self = getContentState();
+  const searchBar = getSearchBarState();
 
-  // Internal
   let triggered = false;
   let lastScrolledTo = 0;
   let scrollElement: HTMLElement;
   let timeoutId: ReturnType<typeof setTimeout>;
-  let lastResultLen = 0;
+  let beforeHasResults = false;
+  let channelRef: ChannelRef;
 
   $effect(() => {
-    if (results.length > 0 && lastResultLen === 0) {
+    if (self.hasResults && !beforeHasResults) {
       scrollElement.scrollTo({ top: 0 });
     }
-    lastResultLen = results.length;
+    beforeHasResults = self.hasResults;
   });
 
-  async function handleChannelClick(channel: ChannelRef): Promise<void> {
-    await getClips(channel, sort);
+  async function handleChannelClick(ref: ChannelRef): Promise<void> {
+    channelRef = ref;
+    await searchBar.searchClips(channelRef);
   }
 
   function handleClipClick(ref: ClipRef): void {
-    clipRef = ref;
+    console.log('click channel', ref);
+    //clipRef = ref;
   }
 
   async function handleScroll(): Promise<void> {
-    if (!channelRef || !scrollElement) return;
+    if (!self.channelSelected || !scrollElement) return;
     const scrolledTo = scrollElement.scrollTop + scrollElement.clientHeight;
     const isGoingDown = scrolledTo > lastScrolledTo;
     const shouldTrigger = scrolledTo >= (scrollElement.scrollHeight - THRESHOLD_PX);
     if (isGoingDown && shouldTrigger && !triggered) {
       triggered = true;
-      await getClips(channelRef, sort);
+      await searchBar.searchClips(channelRef);
       timeoutId = setTimeout(() => { triggered = false; }, COOLDOWN_MS);
     }
     lastScrolledTo = scrolledTo;
@@ -64,9 +57,9 @@
   });
 </script>
 
-<div class="w-full min-h-0 flex flex-col flex-1 m-3 {hasResults ? '' : 'hidden'}">
-  {#if channelRef}
-  <Sort bind:sort />
+<div class="w-full min-h-0 flex flex-col flex-1 m-3 {self.hasResults ? '' : 'hidden'}">
+  {#if self.channelSelected}
+  <Sort bind:sort={self.sort} />
   {/if}
   <div
     bind:this={scrollElement}
@@ -74,14 +67,14 @@
     class="outline overflow-y-auto size-full flex-1 bg-gray-950"
   >
     <div class="m-3 p-2 items-center gap-2 grid grid-cols-2
-                md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+      md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
     >
-      {#if !channelRef}
-        {#each results as channel}
+      {#if !self.channelSelected}
+        {#each self.channels as channel}
         <Channel channel={(channel as ChannelObject)} handleClick={handleChannelClick} />
         {/each}
       {:else}
-        {#each results as clip}
+        {#each self.clips as clip}
         <Clip clip={(clip as ClipObject)} handleClick={handleClipClick} />
         {/each}
       {/if}
