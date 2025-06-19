70ac78f3dc4d91ef1b039fcede1cfb7262ecb8a3
Fedecas <elfefecabjsape@gmail.com>
2025-06-19 18:48:03 -0300

    refactor: clean up imports and remove unused code in App component

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-  import type { ChannelObject, ClipObject, ChannelRef, ClipRef, SortType } from '$lib/types';
-  import VideoPlayer from '$lib/video-player/VideoPlayer.svelte';
-  import { searchClips } from '$lib/api';
-  import ContentCmp from '$lib/content/Content.svelte';
-  import SearchBarCmp from '$lib/SearchBar.svelte';
-  import Message from '$lib/Message.svelte';
+  //import VideoPlayer from '$lib/video-player/VideoPlayer.svelte';
+  //import Message from '$lib/Message.svelte';
+  import Content from '$lib/content/Content.svelte';
   import Header from '$lib/Header.svelte';
-  import { notif } from './notifications';
+  import SearchBar from '$lib/SearchBar.svelte';
 
-  import { SearchBarState, setSearchBarState } from './SearchBarState.svelte';
-  import { ContentState, setContentState } from './ContentState.svelte';
   import { ChannelState, setChannelState } from './ChannelState.svelte';
   import { ClipState, setClipState } from './ClipState.svelte';
+  import { ContentState, setContentState } from './ContentState.svelte';
+  import { SearchBarState, setSearchBarState } from './SearchBarState.svelte';
 
   const CHS = new ChannelState();
   const CLS = new ClipState();
@@ -22,45 +19,6 @@
   setClipState(CLS);
   setContentState(CS);
   setSearchBarState(SBS);
-
-  /*
-  $effect(() => {
-    if (sort !== lastSort && !!channelRef) {
-      (async () => {
-        await handleSearchClips(channelRef, sort);
-      })();
-    }
-    lastSort = sort;
-  });
-
-  async function handleSearchClips(channel: ChannelRef, sort: SortType): Promise<void> {
-    if (endReached) {
-      notif.success('No more clips');
-      return;
-    }
-    SB.searching = true;
-
-    if (channelRef !== channel) {
-      channelRef = channel;
-      results = [];
-    } else if (sort != lastSort) {
-      channelRef.nextCursor = '';
-      results = [];
-    }
-
-    const slug = channelRef.slug;
-    const cursor = channelRef.nextCursor || '';
-    const res = await searchClips(slug, cursor, sort);
-    const clips = results as ClipObject[];
-    results = [
-      ...clips,
-      ...res.clips.filter(c1 => !clips.some(c2 => c1.id === c2.id))
-    ];
-    channelRef.nextCursor = res.nextCursor;
-
-    SB.searching = false;
-  }
-  */
 </script>
 
 <main class="flex flex-col items-center overflow-hidden
@@ -69,15 +27,12 @@
 >
   <div class="w-full flex flex-col items-center">
     <Header hasResults={CS.hasResults} />
-    <SearchBarCmp />
+    <SearchBar />
   </div>
-  <ContentCmp />
+  <Content />
   <!--
   {#if clipRef}
   <VideoPlayer bind:ref={clipRef} bind:downloads />
   {/if}
   -->
-  {#if SBS.firstSearch && !CS.hasResults && !SBS.searching}
-  <Message text="no results found :(" />
-  {/if}
 </main>
